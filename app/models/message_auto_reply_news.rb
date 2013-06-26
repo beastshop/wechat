e6d246dda2848ccda00ec1b4946dfc3c7343a360
Bruce <zth5906@gmail.com>
2013-06-27 00:55:50 +0800

    merge

--- app/models/message_auto_reply_news.rb
+++ app/models/message_auto_reply_news.rb
@@ -1,4 +1,5 @@
 class MessageAutoReplyNews < ActiveRecord::Base
   attr_accessible :article_count, :content
   has_and_belongs_to_many :message_keywords
+  has_many :message_auto_reply_news_articles
 end
