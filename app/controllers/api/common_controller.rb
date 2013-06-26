b62e94cb201e496c309dded8a78d4d13d9d8edd5
Bruce <zth5906@gmail.com>
2013-06-27 01:13:11 +0800

    test

--- app/controllers/api/common_controller.rb
+++ app/controllers/api/common_controller.rb
@@ -44,7 +44,7 @@ class Api::CommonController < Api::ApplicationController
 						news_article.title = article.title
 						news_article.description = article.description
 						news_article.pic_url = request.protocol << request.host_with_port << article.pic_url.to_s
-						news_article.url = article.url
+						news_article.url = news_article.pic_url
 						@message.message_send_news_articles << news_article
 					end
 					p @message
