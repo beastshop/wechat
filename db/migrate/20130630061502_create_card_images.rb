0c7b3a143837d2fe4c7cb31fc973e35c2d9107d9
liuzelei <liuzelei@gmail.com>
2013-06-30 14:27:11 +0800

    update model

--- db/migrate/20130630061502_create_card_images.rb
+++ db/migrate/20130630061502_create_card_images.rb
@@ -1,13 +1,13 @@
-class CreateCardPictures < ActiveRecord::Migration
+class CreateCardImages < ActiveRecord::Migration
   def change
     create_table :card_pictures do |t|
       t.string :order_no
       t.string :wechat_user_open_id
       t.string :title
       t.integer :card_id
-      t.string :image_file_name
-      t.string :image_content_type
-      t.integer :image_file_size
+      t.string :picture_file_name
+      t.string :picture_content_type
+      t.integer :picture_file_size
 
       t.timestamps
     end
