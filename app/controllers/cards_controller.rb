eb9919e92627f6bcce915567bcef2e646af4b448
Bruce <zth5906@gmail.com>
2013-07-03 00:01:57 +0800

    test

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,6 @@
 class CardsController < ApplicationController
 	def show
 		@cards = Card.where(:order_no => params[:id])
-		@card_images = CardImages.where(:order_no => params[:id])
+		@card_images = CardImage.where(:order_no => params[:id])
 	end
 end
