18a422df0441e22bffdbfcb3d48a13d0ea1600d5
Bruce <zth5906@gmail.com>
2013-07-02 22:49:41 +0800

    test

--- app/controllers/api/common_controller.rb
+++ app/controllers/api/common_controller.rb
@@ -16,81 +16,82 @@ class Api::CommonController < Api::ApplicationController
 
 		main_tree = "1.查询订单 \x0A2.录入祝福 \x0A"
 
-		user = MagentoCustomer.where(:wechat_user_open_id => @message.to_user_name).first
+		# user = MagentoCustomer.where(:wechat_user_open_id => @message.to_user_name).first
 
 		
-		p user
-		case params[:xml][:MsgType]
-		when "text"
-			msg_text = params[:xml][:Content]
-			p @message
-			case msg_text
-			when "0"
-				@message.content = main_tree
-			when "1"
-				unless user.nil?
-					orders = TheBeast::Order.get_list(user.user_id)
-					result = ""
-					if orders.nil? || orders.size == 0
-						result = "最近没有订单"
-					else
-						orders.each do | order_item |
-							order = TheBeast::Order.get(order_item.order_id)
-							result <<  "订单号: " << order.order_id << "\x0A" << "地址: " << order.address << "\x0A" << "备注: " << order.note << "\x0A\x0A"
-						end
-					end
-					@message.content = result + "这是订单啊 亲"
-				else
-					@message.content = "您还未绑定TheBeast账号，<a href='http://ds.12doo.com/the_beast/sessions/new?open_id=" + @message.to_user_name.to_s + "'>绑定</a> \x0A"
-				end
-			when "2"
-				@message.content = "请输入祝福的文字或图片,输入 0  退出录入祝福"
-			else
-				unless user.nil?	
-					card = Card.new
-					card.content = msg_text
-					card.order_no = TheBeast::Order.get_list(user.user_id)[0].order_id
-				end
+		# p user
+		# case params[:xml][:MsgType]
+		# when "text"
+		# 	msg_text = params[:xml][:Content]
+		# 	p @message
+		# 	case msg_text
+		# 	when "0"
+		# 		@message.content = main_tree
+		# 	when "1"
+		# 		unless user.nil?
+		# 			orders = TheBeast::Order.get_list(user.user_id)
+		# 			result = ""
+		# 			if orders.nil? || orders.size == 0
+		# 				result = "最近没有订单"
+		# 			else
+		# 				orders.each do | order_item |
+		# 					order = TheBeast::Order.get(order_item.order_id)
+		# 					result <<  "订单号: " << order.order_id << "\x0A" << "地址: " << order.address << "\x0A" << "备注: " << order.note << "\x0A\x0A"
+		# 				end
+		# 			end
+		# 			@message.content = result + "这是订单啊 亲"
+		# 		else
+		# 			@message.content = "您还未绑定TheBeast账号，<a href='http://ds.12doo.com/the_beast/sessions/new?open_id=" + @message.to_user_name.to_s + "'>绑定</a> \x0A"
+		# 		end
+		# 	when "2"
+		# 		@message.content = "请输入祝福的文字或图片,输入 0  退出录入祝福"
+		# 	else
+		# 		unless user.nil?	
+		# 			card = Card.new
+		# 			card.content = msg_text
+		# 			card.order_no = TheBeast::Order.get_list(user.user_id)[0].order_id
+		# 		end
 
-				@message.content = "无法理解您的输入，请重新按菜单输入 \x0A" + main_tree
-			end
-			@message.content = "kao"
-			render :xml, :template => 'api/message_text'
+		# 		@message.content = "无法理解您的输入，请重新按菜单输入 \x0A" + main_tree
+		# 	end
+		# 	@message.content = "kao"
+		# 	render :xml, :template => 'api/message_text'
 			##-------------------------------   关键字匹配代码 Don't Remove -----------------------------------------------------------------------
-			# @mkw = MessageKeyword.where("locate(content,'#{msg_text}')>0").first
+			msg_text = params[:xml][:Content]
+			@mkw = MessageKeyword.where("locate(content,'#{msg_text}')>0").first
 
-			# if !@mkw.nil?
-			# 	if @mkw.message_auto_reply_texts.size > 0
-			# 		@message.content = @mkw.message_auto_reply_texts.first.content
-			# 		render :xml, :template => 'api/message_text'
-			# 	elsif @mkw.message_auto_reply_musics.size > 0
-			# 		@message = MessageSendMusic.new
-			# 		@message.to_user_name = params[:xml][:FromUserName]
-			# 		@message.from_user_name = params[:xml][:ToUserName]
-			# 		@message.create_time = Time.now
-			# 		@message.music_url = request.protocol + request.host_with_port + @mkw.message_auto_reply_musics.first.music_url.to_s
-			# 		@message.hq_music_url = request.protocol + request.host_with_port + @mkw.message_auto_reply_musics.first.hq_music_url.to_s
-			# 		p @message
-			# 		render :xml, :template => 'api/message_music'
-			# 	elsif @mkw.message_auto_reply_news.size > 0
-			# 		@message = MessageSendNews.new
-			# 		@message.to_user_name = params[:xml][:FromUserName]
-			# 		@message.from_user_name = params[:xml][:ToUserName]
-			# 		@message.create_time = Time.now
-			# 		@mkw.message_auto_reply_news.first.message_auto_reply_news_articles.each do | article |
-			# 			news_article = MessageSendNewsArticle.new
-			# 			news_article.title = article.title
-			# 			news_article.description = article.description
-			# 			news_article.pic_url = request.protocol + request.host_with_port + article.pic_url.to_s
-			# 			news_article.url = article.url
-			# 			@message.message_send_news_articles << news_article
-			# 		end
-			# 		p @message
-			# 		render :xml, :template => 'api/message_news'
-			# 	end
-			# else
-			# 	render :xml, :template => 'api/message_text'
-			# end
+			if !@mkw.nil?
+				if @mkw.message_auto_reply_texts.size > 0
+					@message.content = @mkw.message_auto_reply_texts.first.content
+					render :xml, :template => 'api/message_text'
+				elsif @mkw.message_auto_reply_musics.size > 0
+					@message = MessageSendMusic.new
+					@message.to_user_name = params[:xml][:FromUserName]
+					@message.from_user_name = params[:xml][:ToUserName]
+					@message.create_time = Time.now
+					@message.music_url = request.protocol + request.host_with_port + @mkw.message_auto_reply_musics.first.music_url.to_s
+					@message.hq_music_url = request.protocol + request.host_with_port + @mkw.message_auto_reply_musics.first.hq_music_url.to_s
+					p @message
+					render :xml, :template => 'api/message_music'
+				elsif @mkw.message_auto_reply_news.size > 0
+					@message = MessageSendNews.new
+					@message.to_user_name = params[:xml][:FromUserName]
+					@message.from_user_name = params[:xml][:ToUserName]
+					@message.create_time = Time.now
+					@mkw.message_auto_reply_news.first.message_auto_reply_news_articles.each do | article |
+						news_article = MessageSendNewsArticle.new
+						news_article.title = article.title
+						news_article.description = article.description
+						news_article.pic_url = request.protocol + request.host_with_port + article.pic_url.to_s
+						news_article.url = article.url
+						@message.message_send_news_articles << news_article
+					end
+					p @message
+					render :xml, :template => 'api/message_news'
+				end
+			else
+				render :xml, :template => 'api/message_text'
+			end
 			##-------------------------------   关键字匹配代码 Don't Remove -----------------------------------------------------------------------
 
 
