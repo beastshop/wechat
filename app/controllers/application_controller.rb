3bce0f3f0f22c3b1ead32afa65203a36dee50c80
Bruce <zth5906@gmail.com>
2013-07-05 21:26:12 +0800

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
       "/message_keywords" # <- Path you want to redirect the user to.
+  end
+
+
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      ""
+    else
+      "application"
+    end
   end
 end
