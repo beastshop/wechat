5eab4e5955719f2a1cce756c7ff77f004fb1a356
Bruce <zth5906@gmail.com>
2013-07-10 23:17:58 +0800

    merge

--- app/controllers/api/common_controller.rb
+++ app/controllers/api/common_controller.rb
@@ -104,15 +104,15 @@ class Api::CommonController < Api::ApplicationController
 
 		case type
 		when "text"
-			MessageReceiveText.save(from_user_name,to_user_name,type,msg_id,create_time,params[:xml][:Content])
+			MessageReceiveText.save(from_user_name,to_user_name,type,msg_id,Time.now,params[:xml][:Content])
 		when "image"
-			MessageReceiveImage.save(from_user_name,to_user_name,type,msg_id,create_time,params[:xml][:PicUrl])
+			MessageReceiveImage.save(from_user_name,to_user_name,type,msg_id,Time.now,params[:xml][:PicUrl])
 		when "voice"
-			MessageReceiveVoice.save(from_user_name,to_user_name,type,msg_id,create_time,params[:xml][:MediaId],params[:xml][:Format],params[:xml][:Recognition])
+			MessageReceiveVoice.save(from_user_name,to_user_name,type,msg_id,Time.now,params[:xml][:MediaId],params[:xml][:Format],params[:xml][:Recognition])
 		when "location"
-			MessageReceiveLocation.save(from_user_name,to_user_name,type,msg_id,create_time,params[:xml][:Scale],params[:xml][:Location_X],params[:xml][:Location_Y],params[:xml][:Label])
+			MessageReceiveLocation.save(from_user_name,to_user_name,type,msg_id,Time.now,params[:xml][:Scale],params[:xml][:Location_X],params[:xml][:Location_Y],params[:xml][:Label])
 		when "event"
-			MessageReceiveEvent.save(from_user_name,to_user_name,type,msg_id,create_time,params[:xml][:Event],params[:xml][:EventKey])
+			MessageReceiveEvent.save(from_user_name,to_user_name,type,msg_id,Time.now,params[:xml][:Event],params[:xml][:EventKey])
 		end
 	end
 
