86f98b33fad2b8c96b52dc89747abe29cfbbc62d
Bruce <zth5906@gmail.com>
2013-07-11 12:20:43 +0800

    merge

--- app/controllers/the_beast/sessions_controller.rb
+++ app/controllers/the_beast/sessions_controller.rb
@@ -9,23 +9,26 @@ class TheBeast::SessionsController < TheBeast::ApplicationController
 	def create
 		customer = TheBeast::Customer.login(params[:email], params[:password])
 		if customer && MagentoCustomer.where(email: params[:email],islocked: false).size == 0
-			
+
+			c = MagentoCustomer.new
+			c.user_id = customer.user_id
+			c.email = customer.email
+			c.cards = []
 
 			if MagentoCustomer.where(email: params[:email],islocked: true).exists?
-				mc = MagentoCustomer.where(email: params[:email],islocked: true).first
-				mc.islocked = false
-				mc.save
-			else
-				c = MagentoCustomer.new
-				c.user_id = customer.user_id
-				c.email = customer.email
-
-				user = WechatUser.new
-				user.name = "from wechat api"
-				user.open_id = params[:open_id]
-				user.magento_customer = c
-				user.save
+				MagentoCustomer.where(email: params[:email],islocked: true).last.cards.each do |card|
+					c.cards << card
+				end
 			end
+			
+			user = WechatUser.new
+			user.name = "from wechat api"
+			user.open_id = params[:open_id]
+			user.magento_customer = c
+			user.save
+
+			
+
 			redirect_to :action => "success"
 		elsif customer && MagentoCustomer.where(email: params[:email],islocked: false).size != 0
 			@message = "已有微信账号绑定此用户，请联系管理员"
