f56a904e5a41809843b586e4dc4c1a5767fdb255
czhang <czhang@12doo.com>
2013-07-13 23:14:33 +0800

    fix partial error

--- app/controllers/api/common_controller.rb
+++ app/controllers/api/common_controller.rb
@@ -42,7 +42,7 @@ class Api::CommonController < Api::ApplicationController
 					if @orders.empty?
 						@message.content = "没有订单"
 					else 
-						render :xml, :template => 'api/orders'
+						render :xml => @orders, :template => 'api/orders'
 					end
 					# result = ""
 					# orders.each do | order_item |
