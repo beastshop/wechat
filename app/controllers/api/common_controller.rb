8ecb451c5e51b2e083e680f17959fea3d7318d4c
czhang <czhang@12doo.com>
2013-07-13 19:24:57 +0800

    try to add log

--- app/controllers/api/common_controller.rb
+++ app/controllers/api/common_controller.rb
@@ -35,7 +35,9 @@ class Api::CommonController < Api::ApplicationController
 					user.save
 					@message.content = main_tree
 				when "1"
+					logger.debug 'Query User #{user.email} Order. Start time #{DateTime.current}'
 					orders = TheBeast::Order.get_list(user.user_id)
+					logger.debug 'Query order done.  End time #{DateTime.current}'
 					result = ""
 					orders.each do | order_item |
 						order = TheBeast::Order.get(order_item.order_id)
