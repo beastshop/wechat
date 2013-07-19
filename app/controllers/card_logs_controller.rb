539b7d093815cca3c6e095ecb5c25625edcb3348
Bruce <zth5906@gmail.com>
2013-07-19 14:55:06 +0800

    push

--- app/controllers/card_logs_controller.rb
+++ app/controllers/card_logs_controller.rb
@@ -3,6 +3,6 @@ class CardLogsController < ApplicationController
 	def index
 		order_no = params[:order_no]
 
-		@card_logs = order_no.nil? ? CardLog.page(params[:page]).per(20) : CardLog.joins(:card).where("cards.order_no" => order_no)
+		@card_logs = order_no.nil? ? CardLog.page(params[:page]).per(20) : CardLog.joins(:card).where("cards.order_no" => order_no).page(params[:page]).per(20) 
 	end
 end
